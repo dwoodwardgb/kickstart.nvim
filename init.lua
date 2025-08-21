d46d6aa9a9b29b1c442703903ce6ff1ca31ad567
David Woodward <david.i.woodward@disneystreaming.com>
2025-08-21 12:12:58 -0400

    Configure telescope better

--- init.lua
+++ init.lua
@@ -352,16 +352,9 @@ require('lazy').setup({
       { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
     },
     config = function()
-      -- Telescope is a fuzzy finder that comes with a lot of different things that
-      -- it can fuzzy find! It's more than just a "file finder", it can search
-      -- many different aspects of Neovim, your workspace, LSP, and more!
-      --
       -- The easiest way to use Telescope, is to start by doing something like:
       --  :Telescope help_tags
-      --
-      -- After running this command, a window will open up and you're able to
-      -- type in the prompt window. You'll see a list of `help_tags` options and
-      -- a corresponding preview of the help.
+      -- You'll see a list of `help_tags` options and a corresponding preview of the help.
       --
       -- Two important keymaps to use while in Telescope are:
       --  - Insert mode: <c-/>
@@ -370,22 +363,19 @@ require('lazy').setup({
       -- This opens a window that shows you all of the keymaps for the current
       -- Telescope picker. This is really useful to discover what Telescope can
       -- do as well as how to actually do it!
-
-      -- [[ Configure Telescope ]]
-      -- See `:help telescope` and `:help telescope.setup()`
       require('telescope').setup {
         defaults = {
-          -- NOTE: these values were picked testing from ~/d/fellowship which has many nested projects
-          -- path_display = {
-          --   truncate = 0,
-          --   shorten = { len = 1, exclude = { 1, 2, -2, -1 } },
-          -- },
+          path_display = {
+            truncate = 0,
+            -- shorten = { len = 1, exclude = { 1, 2, -2, -1 } },
+          },
           borderchars = { '─', '', '', '', '', '', '', '' },
           layout_strategy = 'vertical',
           layout_config = {
             horizontal = {
               width = 0.999,
               height = 0.999,
+              -- NOTE: this was set assuming max width is 110 cols in Ghosty with font-family=BlexMono Nerd Font Mono and font-size=19
               preview_width = 0.65,
             },
             vertical = {
@@ -413,21 +403,24 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+      -- TODO: figure out how to search exactly by case etc (non fuzzy)
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader><leader>', function()
+        builtin.buffers(require('telescope.themes').get_dropdown {
+          previewer = false,
+        })
+      end, { desc = '[ ] Find existing buffers' })
       vim.keymap.set('n', '<leader>st', builtin.colorscheme, { desc = '[S]earch [T]hemes' })
 
       -- TODO: add builtin for cached searches/pickers see https://www.reddit.com/r/neovim/comments/phndpv/can_telescope_remember_my_last_search_result/
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
-        -- You can pass additional configuration to Telescope to change the theme, layout, etc.
         builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-          -- winblend = 10,
           previewer = false,
         })
       end, { desc = '[/] Fuzzily search in current buffer' })
