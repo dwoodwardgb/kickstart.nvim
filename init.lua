d352e8ba014b790937566bcaf55d2231c2ce669a
David Woodward <david.i.woodward@disneystreaming.com>
2025-08-21 15:35:44 -0400

    Use better file picker

--- init.lua
+++ init.lua
@@ -401,7 +401,11 @@ require('lazy').setup({
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>sf', function()
+        builtin.find_files(require('telescope.themes').get_dropdown {
+          previewer = false,
+        })
+      end, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       -- TODO: figure out how to search exactly by case etc (non fuzzy)
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
