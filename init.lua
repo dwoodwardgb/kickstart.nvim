59149991c70174767063f57f6b45a46e0903225c
David Woodward <david.i.woodward@disneystreaming.com>
2025-08-21 12:14:21 -0400

    add todos

--- init.lua
+++ init.lua
@@ -726,6 +726,7 @@ require('lazy').setup({
     opts = {
       keymap = {
         -- enter to select, see https://cmp.saghen.dev/configuration/keymap.html#presets for details
+        -- TODO: figure out how to trigger with ctrl-space in insert mode
         preset = 'enter',
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
